843704716cd77b7a696ccdf4ce500a6fb283f1b8
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-01-15 02:25:19 +0600

    create all medicine and specific medicine API

---
+++ index.js
@@ -0,0 +1,62 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 4000;
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.73pqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const medicineCollection = client.db("medicineDB").collection("medicine");
+
+    // app.post("/medicine", async (req, res) => {
+    //   const query = req.body;
+    //   const result = await medicineCollection.insertOne(query);
+    //   res.send(result);
+    // });
+
+    app.get("/allMedicine", async (req, res) => {
+      const result = await medicineCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/medicine/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = { category_name : category };
+      const result = await medicineCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Medical Health Care is Available");
+});
+
+app.listen(port, () => {
+  console.log("Medical Server is running on the port", port);
+});
